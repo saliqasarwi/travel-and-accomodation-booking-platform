e7d130da2c7f3037dbd14d20dc85e3e13620e037
saliqasarwi <sali.qasarwi@gmail.com>
2026-02-04 03:30:14 +0200

    test(app): setup vitest test runner and rtl config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import path from "path";
 export default defineConfig({
@@ -11,5 +11,10 @@ export default defineConfig({
       "@styles": path.resolve(__dirname, "src/styles"),
       "@assets": path.resolve(__dirname, "src/assets"),
     },
+  },
+  test: {
+    environment: "jsdom",
+    setupFiles: ["./src/setupTests.ts"],
+    globals: true,
   },
 });
