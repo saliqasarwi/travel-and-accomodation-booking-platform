aafa8bd7d806f76e817686899c2eea4b85b122f4
saliqasarwi <sali.qasarwi@gmail.com>
2026-02-22 02:03:29 +0200

    feat(checkout):add checkout stepper component

--- src/features/checkout/pages/CheckoutPage.tsx
+++ src/features/checkout/pages/CheckoutPage.tsx
@@ -1,3 +1,11 @@
+import { useState } from "react";
+import CheckoutStepper from "../components/CheckoutStepper";
+
 export default function CheckoutPage() {
-  return <h1>Checkout</h1>;
+  const [activeStep] = useState(0);
+  return (
+    <div>
+      <CheckoutStepper activeStep={activeStep} />
+    </div>
+  );
 }
