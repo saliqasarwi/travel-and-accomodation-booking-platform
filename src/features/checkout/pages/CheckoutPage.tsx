0e68fe57c2d522d8ecb1cb3c637bd697746a5cf5
saliqasarwi <sali.qasarwi@gmail.com>
2026-02-22 03:50:37 +0200

    feat(checkout):wire stepper navigation in checkout page

--- src/features/checkout/pages/CheckoutPage.tsx
+++ src/features/checkout/pages/CheckoutPage.tsx
@@ -1,11 +1,32 @@
-import { useState } from "react";
+import { Button, Card, CardContent, Stack } from "@mui/material";
 import CheckoutStepper from "../components/CheckoutStepper";
-
+import { useState } from "react";
+const stepsCount = 3;
 export default function CheckoutPage() {
-  const [activeStep] = useState(0);
+  const handleNext = () =>
+    setActiveStep((s) => Math.min(s + 1, stepsCount - 1));
+  const handleBack = () => setActiveStep((s) => Math.max(s - 1, 0));
+
+  const [activeStep, setActiveStep] = useState(0);
   return (
-    <div>
+    <Stack spacing={3}>
       <CheckoutStepper activeStep={activeStep} />
-    </div>
+      <Card>
+        <CardContent>
+          {activeStep === 0 && <div>Guest info step</div>}
+          {activeStep === 1 && <div>Payment step</div>}
+          {activeStep === 2 && <div>Special requests + review step</div>}
+        </CardContent>
+      </Card>
+
+      <Stack direction="row" justifyContent="space-between">
+        <Button onClick={handleBack} disabled={activeStep === 0}>
+          Back
+        </Button>
+        <Button variant="contained" onClick={handleNext}>
+          Next
+        </Button>
+      </Stack>
+    </Stack>
   );
 }
